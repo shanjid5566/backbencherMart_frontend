d1b5d2beead75062006cff043acda47dbbb1062c
Shanjid Ahmed Rafi <shanjidahmed66@gmail.com>
2026-02-04 00:24:16 +0600

    Add Lenis smooth scrolling integration and styles

---
+++ src/hooks/useSmoothScroll.js
@@ -0,0 +1,35 @@
+import { useEffect } from 'react'
+import Lenis from 'lenis'
+
+/**
+ * Hook to initialize Lenis smooth scrolling
+ * Call this once at the app root level
+ */
+const useSmoothScroll = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      touchMultiplier: 2,
+      infinite: false,
+    })
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+    // Cleanup on unmount
+    return () => {
+      lenis.destroy()
+    }
+  }, [])
+}
+
+export default useSmoothScroll
--- src/layouts/homeLayout/HomeLayout.jsx
+++ src/layouts/homeLayout/HomeLayout.jsx
@@ -3,10 +3,14 @@ import { Outlet } from 'react-router'
 import Announcement from '../../components/Announcement'
 import Header from '../../components/Header'
 import useThemeEffect from '../../hooks/useThemeEffect'
+import useSmoothScroll from '../../hooks/useSmoothScroll'
 
 const HomeLayout = () => {
   // Apply dark class to root element based on Redux state
   useThemeEffect()
+  
+  // Initialize Lenis smooth scrolling
+  useSmoothScroll()
 
   return (
     <div className="min-h-screen bg-white dark:bg-dark-bg transition-colors duration-300">
